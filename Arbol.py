16a87dd2579a0106af49606b01233e33494ed625
Aribeth14 <aribeth1410@gmail.com>
2025-07-27 05:23:48 -0500

    Update Arbol Sumar puntos .py

--- Arbol.py
+++ Arbol.py
@@ -6,13 +6,39 @@ class NodoHabilidad:
     self.izquierda= None
     self.derecha = None
 
-def sumar_puntos(nodo):
+class ArbolHabilidades:
+  def __init__(self):
+    self.raiz=None
+
+  def Manual(self):
+  "Arbol con tres habilidades fijas"
+  self.raiz=NodoHabilidad("Ataque Rapido",2)
+  self.raiz.izquierdo=NodoHabilidad("Defensa fuerte",3)
+  self.raiz.derecha=NodoHabilidad("Agilidad Extrema",1)
+  
+  def sumar_puntos(nodo):
   if nodo is None:
     return 0
   return nodo.puntos +sumar_puntos(nodo.izquierda)+sumar_puntos(nodo.derecha)
 
-raiz=NodoHabilidad("Ataque Rapido",2)
-raiz.izquierda=NodoHabilidad("Defensa Fuerte",3)
-raiz.derecha=NodoHabilidad("Agilidad Extrema",1)
+  def mostrar(self,nodo=None,nivel=0):
+    if nivel==0 and nodo is None:
+      nodo = self.raiz
+    if nodo is None:  
+      return
+   
+  print(" " *(nivel *4)+f"´{nodo.nombre} ({nodo.puntos})")
+  self.mostrar(nodo.izquierdo,nivel +1)
+  self.mostrar(nodo.derecho,nivel+1)
 
-print("Suma total de puntos: "sumar_puntos(raiz))
+arbol=ArbolHabilidades()
+arbol.Manual()
+
+print("Arbol de habilidades")
+arbol.mostrar()
+
+print("\nTotal de puntos:",arbol.sumar_puntos())
+
+  
+
+
