d1f32bccc40c15be4d710e3910ea8b5cd82a8245
patiño <rojasjosue55a@gmail.com>
2025-07-27 23:04:19 -0500

    Torneo automatico completo unido a Arbol

--- Torneo.py
+++ Torneo.py
@@ -1,15 +1,35 @@
+from collections import deque
+from Arbol import simular_combate  
+
 def torneo(ninjas):
-  while len(ninjas) > 1:
-    n1 = ninjas.pop(0)
-    n2 = ninjas.pop(0)
-    print(f"{n1} VS {n2}")
-    ganador = n1
-    print(f"Gana{ganador}")
-    ninjas.append(ganador)
-  print(f"El ganador y finalista es: {ninjas[0]}")
+    print("=== Inicio Torneo Ninja ===")
+    participantes = ninjas[:16]  
 
-terneo(ninjas)
+    ronda_num = 1
+    cola = deque(participantes)
 
+    while len(cola) > 1:
+        print(f"\n>>> Ronda {ronda_num} ({len(cola)} participantes)")
+        siguiente_ronda = deque()
 
-  
+        while len(cola) > 1:
+            ninja1 = cola.popleft()
+            ninja2 = cola.popleft()
+
+            print(f"\nCombate: {ninja1['nombre']} vs {ninja2['nombre']}")
+            resultado, ganador = simular_combate(ninja1, ninja2)
+            print(resultado)
+
+            for ninja in participantes:
+                if ninja["nombre"] == ganador:
+                    siguiente_ronda.append(ninja)
+                    break
+
+        cola = siguiente_ronda
+        ronda_num += 1
+
+    campeon = cola[0]
+    print(f"\n🏆 Campeón absoluto del torneo: {campeon['nombre']} 🏆")
+    return campeon["nombre"]
+
 
